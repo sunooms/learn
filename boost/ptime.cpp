85968958927201701f7f92683bcbe8dd0bccb619
xgmlovebee <xgmlovebee@126.com>
2016-12-27 15:14:24 +0800

    boost ptime

--- boost/ptime.cpp
+++ boost/ptime.cpp
@@ -4,14 +4,18 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/foreach.hpp>
 
 
+using namespace std;
 using namespace boost::posix_time;
 using namespace boost::gregorian;
+using namespace boost;
 
 int main()
 {
-    long days = 0;
+    long dis_days = 0;
     ptime ptNow( second_clock::local_time() );
     ptime ptpost = from_time_t(1478405472);
     ptime p1=second_clock::local_time();
@@ -19,8 +23,35 @@ int main()
     std::cout << to_simple_string( p1 ) << std::endl;
     std::cout << to_simple_string( ptpost ) << std::endl;
 
-    days = boost::lexical_cast<long>(ptNow.date() - ptpost.date());
+    dis_days = boost::lexical_cast<long>(ptNow.date() - ptpost.date());
 
-    printf("now days :%ld\n", days);
+    printf("now dis_days :%ld\n", dis_days);
+
+    ptime ptpost2 = from_time_t(1481046648);
+    ptime ptpost3 = from_time_t(1481056648);
+
+    dis_days = boost::lexical_cast<long>(ptpost2.date() - ptpost3.date());
+    printf("now dis_days :%ld\n", dis_days);
+
+
+    date d1 = day_clock::local_day();
+    date d2 = d1 -days(2);
+
+    // print YYYMMDD
+    std::string strTime =  boost::gregorian::to_iso_string(boost::gregorian::day_clock::local_day());
+    cout << strTime.c_str() << endl;
+
+    std::string strTime2 =  boost::gregorian::to_iso_string(d2);
+    cout << strTime2.c_str() << endl;
+
+    // print yyymmdd-HH:MM:SS
+    std::string strTime3 = boost::posix_time::to_iso_string(boost::posix_time::second_clock::local_time());
+    // 这时候strTime里存放时间的格式是YYYYMMDDTHHMMSS，日期和时间用大写字母T隔开了
+    int pos = strTime3.find('T');
+    strTime3.replace(pos,1,std::string("-"));
+    strTime3.replace(pos + 3,0,std::string(":"));
+    strTime3.replace(pos + 6,0,std::string(":"));
+
+    std::cout << strTime3.c_str() << std::endl;
     return 0;
 }
