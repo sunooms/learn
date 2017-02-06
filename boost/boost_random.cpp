3ecc0fdb4d893a643318970bc6400b443d6a19e9
sunooms <xgmlovebee@126.com>
2017-02-06 16:15:41 +0800

    add boost random

---
+++ boost/boost_random.cpp
@@ -0,0 +1,20 @@
+#include <boost/random.hpp>
+#include <boost/random.hpp>
+#include <iostream>
+#include <time.h>
+
+int main()
+{
+    //boost::mt19937 gen(time(0));
+    boost::mt19937 gen;
+    boost::uniform_int<> dist(1,6);
+    //boost::variate_generator<boost::mt19937 &, boost::uniform_int<> > die(gen,dist);
+    boost::variate_generator<boost::mt19937, boost::uniform_int<> > die(boost::mt19937(time(0)), dist);
+
+
+    for( int i=0; i<30; ++i ) {
+        std::cout<<die() <<std::endl;
+
+    }
+
+}
