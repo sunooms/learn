f60b78fc9e8818957c0da3e318acf6681e92acd2
sunooms <xgmlovebee@126.com>
2017-03-08 15:25:27 +0800

    add multithreads python demo

--- boost/boost_random.cpp
+++ boost/boost_random.cpp
@@ -7,7 +7,7 @@ int main()
 {
     //boost::mt19937 gen(time(0));
     boost::mt19937 gen;
-    boost::uniform_int<> dist(1,6);
+    boost::uniform_int<> dist(1,30);
     //boost::variate_generator<boost::mt19937 &, boost::uniform_int<> > die(gen,dist);
     boost::variate_generator<boost::mt19937, boost::uniform_int<> > die(boost::mt19937(time(0)), dist);
 
