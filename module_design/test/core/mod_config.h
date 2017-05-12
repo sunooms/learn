c4ff6d798de70927fdce7bd847a55d98bea70ffa
sunooms <xgmlovebee@126.com>
2017-05-12 13:30:36 +0800

    add more

--- module_design/test/core/mod_config.h
+++ module_design/test/core/mod_config.h
@@ -85,4 +85,52 @@ protected:
     std::string flows_name_;
 };
 
+
+inline const std::string& ComponentConfig::class_name() const{
+    return class_name_;
+}
+
+inline const std::string& ComponentConfig::mod_name() const{
+    return mod_name_;
+}
+
+inline const std::string& FlowConfig::class_name() const{
+    return class_name_;
+}
+
+inline const std::string& FlowConfig::mod_name() const{
+    return mod_name_;
+}
+
+inline const std::vector<ComponentConfig*>& FlowConfig::components() const{
+    return components_;
+}
+
+inlineconst std::map<std::string, ComponentConfig*>& FlowConfig::components_map() const{
+    return components_map_;
+}
+
+inline const std::string& ComponentConfig::ConfigData(std::string param_name) const{
+    static const std::string null_string("");
+    std::map<std::string, std::string>::const_iterator it=conf_data_.find(param_name);
+    return (it!=conf_data.end())?it->second:null_string;
+}
+
+inline int ComponentConfig::GetHookOrder(std::string hook_name) const{
+    std::map<std::string, int>::const_iterator it=hook_order_.find(hook_name);
+    return (it!=hook_order_.end())?it->second:hook_default_order_;
+}
+
+inline const std::string& ModuleConfig::flows_name() const{
+    return flows_name_;
+}
+
+inline const std::map<std::string, FlowConfig>& ModuleConfig::flows() const{
+    return flows_;
+}
+
+inline const std::vector<std::string>& ModuleConfig::modules() const{
+    return modules_;
+}
+
 #endif // CORE_MOD_CONFIG_H__
